3337a0ce0d065f0d0db3f578262a52bf89fcf91b
hemglory <39890987+hemglory@users.noreply.github.com>
2023-05-31 11:06:39 -0700

    Update streamlit_app.py
    Fixed the closing bracket related issue.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ streamlit.title('My parents new healthy diner')
 streamlit.header('DINNER MENU')
 Streamlit.text('Avakaya Pachadi')
 streamlit.text('Gongura Pachadi')
-Streamlit.text('Maagaya Pachadi'
+Streamlit.text('Maagaya Pachadi')
