1ccd31268608c3a86b05ad0070e72938e3acc2da
hemglory <39890987+hemglory@users.noreply.github.com>
2023-05-31 11:17:49 -0700

    Update streamlit_app.py
    Added icons for the menu items and added an extra item.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('DINNER MENU')
-streamlit.text('Avakaya Pachadi')
-streamlit.text('Gongura Pachadi')
-streamlit.text('Maagaya Pachadi')
+streamlit.text('🥣 Avakaya Pachadi')
+streamlit.text('🥗 Gongura Pachadi')
+streamlit.text('🥑 Maagaya Pachadi')
+streamlit.text('🍞 Avocado Pachadi')
