79a19b2a831844e25945d6370d324959d2b28474
hemglory <39890987+hemglory@users.noreply.github.com>
2023-05-31 11:05:54 -0700

    Update streamlit_app.py
    Added a header and three texts.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('DINNER MENU')
+Streamlit.text('Avakaya Pachadi')
+streamlit.text('Gongura Pachadi')
+Streamlit.text('Maagaya Pachadi'
