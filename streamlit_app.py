fc871ac79e45f07db95819e2a63413d4dea838f6
hemglory <39890987+hemglory@users.noreply.github.com>
2023-05-31 17:53:03 -0700

    Update streamlit_app.py
    Delete json data display

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ streamlit.dataframe(fruits_to_show)
 # New section to display the frutiyvice response on the page
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
 # Use pandas to normalize and story in a variable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the normalized data in a dataframe
