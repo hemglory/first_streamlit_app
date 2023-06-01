d64c8ea1a598445307d0de22b03e3cb0fc457ea7
hemglory <39890987+hemglory@users.noreply.github.com>
2023-06-01 00:24:56 -0700

    Update streamlit_app.py
    Added definition of get_fruitvice_data function

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
-
 #import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('DINNER MENU')
@@ -23,6 +21,10 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+def get_fruityvice_data(this_fruit_choice);
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
 
 # New section to display the frutiyvice response on the page
 streamlit.header("Fruityvice Fruit Advice!")
@@ -31,9 +33,9 @@ try:
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
+
 except URLError as e:
     streamlit.error()
 # dont run anything past here while we troubleshoot
